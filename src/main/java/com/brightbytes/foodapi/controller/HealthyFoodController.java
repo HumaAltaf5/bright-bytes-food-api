1a120c2bd14c3524abcde22dc59fd4a64221b0eb
HumaAltaf5 <huma.altaf@gmail.com>
2022-02-25 10:51:11 +0000

    Adding first test to display the string "Welcome to the Healthy Food API" for end point "/"

--- src/main/java/com/brightbytes/foodapi/controller/HealthyFoodController.java
+++ src/main/java/com/brightbytes/foodapi/controller/HealthyFoodController.java
@@ -1,7 +1,13 @@
 package com.brightbytes.foodapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthyFoodController {
+
+    @GetMapping(value = "/")
+    public String home() {
+        return "Welcome to the Healthy Food API";
+    }
 }
