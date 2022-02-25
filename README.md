195866c04935b04e381014befe0cb51e43b22de0
HumaAltaf5 <huma.altaf@gmail.com>
2022-02-25 10:31:22 +0000

    Initial commit to create a REST API for displaying Healthy Meals using Spring Boot Framework

---
+++ README.md
@@ -0,0 +1,3 @@
+Healthy Food API
+
+This API will allow users to specify calories and display 3 healthy meals amounting to the total number of calories input by the user.
---
+++ src/main/java/com/brightbytes/foodapi/controller/HealthyFoodController.java
@@ -0,0 +1,7 @@
+package com.brightbytes.foodapi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthyFoodController {
+}
