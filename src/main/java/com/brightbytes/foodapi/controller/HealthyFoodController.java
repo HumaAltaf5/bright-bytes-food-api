0bfe6af9d6b1dba3d8dad779935ca0e50c865277
HumaAltaf5 <huma.altaf@gmail.com>
2022-02-26 20:56:52 +0000

    Added @CrossOrigin annotation for resource sharing to make html page to display jason object

--- src/main/java/com/brightbytes/foodapi/controller/HealthyFoodController.java
+++ src/main/java/com/brightbytes/foodapi/controller/HealthyFoodController.java
@@ -5,13 +5,14 @@ import com.brightbytes.foodapi.service.HealthyFoodServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-
+@CrossOrigin
 public class HealthyFoodController {
 
     @Autowired
