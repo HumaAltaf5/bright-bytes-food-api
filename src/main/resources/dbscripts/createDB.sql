d96e146743ac82a7c81ae533ba59e4cb31d0f80e
Hin Chi WONG <phantomcase@gmail.com>
2022-03-03 15:39:05 +0000

    update DB create statement and added 20 samples data

--- src/main/resources/dbscripts/createDB.sql
+++ src/main/resources/dbscripts/createDB.sql
@@ -1,2 +1,14 @@
 CREATE DATABASE IF NOT EXISTS  brightbytes;
 USE brightbytes;
+
+create table dish (
+id int NOT NULL AUTO_INCREMENT,
+name VARCHAR(255) NOT NULL UNIQUE,
+calories int,
+mealtype VARCHAR(40),
+category VARCHAR(40),
+recipelink TEXT,
+Imagelink TEXT,
+allergens VARCHAR(15),
+PRIMARY KEY (id)
+);
